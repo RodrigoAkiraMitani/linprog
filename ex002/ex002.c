36a4dd57d8803cf94b179f733255feb57e3bebc3
Temp CAS <temp.cas@CAS411595MAC20.local>
2016-03-04 22:06:35 -0300

    inclusao dos exs

---
+++ ex002/ex002.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf ("vida longa e prospera!\n");
+}
+
---
+++ ex003/ex003.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+	printf ("Que a forca esteja com voce!\n");
+}
